6e8db2570acaae998a747594b7adfaab3da4d9c0
Alexandar Dimitrov <40325490+AlexDimitro0v@users.noreply.github.com>
2019-07-31 00:46:11 +0200

    Python Projects

--- Python/Python-Fundamentals/6.OOP Princples/1.Person.py
+++ Python/Python-Fundamentals/6.OOP Princples/1.Person.py
@@ -1,7 +1,7 @@
 # _____________________________________________________________________________________________________________________
 
 
-# Proper Python getters and setters solution
+# Proper Python getters and setters solution  -> props is a shortcut for displaying all the getter and setters
 class Person:
     def __init__(self, name, age):
         self.name = name
@@ -60,43 +60,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-# _____________________________________________________________________________________________________________________
-# another way
-
-class Person:
-    def __init__(self, name, age):
-        self.name = self.set_name(name)
-        self.age = self.set_age(age)
-
-    @staticmethod
-    def set_age(age):
-        if age < 0:
-            raise Exception("Age must be positive!")
-        else:
-            return age
-
-    @staticmethod
-    def set_name(name):
-        if len(name) < 3:
-            raise Exception("Name's length should not be less than 3 symbols!")
-        else:
-            return name
-
-    def __str__(self):
-        return f"Name: {self.name}, Age: {self.age}"
-
-
-class Child(Person):
-    def __init__(self, name, age):
-        super().__init__(name, age)
-
-    @staticmethod
-    def set_age(age):
-        if age < 0:
-            raise Exception("Age must be positive!")
-        elif age > 15:
-            raise Exception("Child's age must be less than 15!")
-        else:
-            return age

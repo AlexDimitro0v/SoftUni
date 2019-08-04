393f64bd597cedbd5f41d152c5756825a8cee0b2
Alexandar Dimitrov <40325490+AlexDimitro0v@users.noreply.github.com>
2019-08-04 11:16:58 +0200

    Python Projects

--- Python/Python-Fundamentals/5.Obejcts and classes/0.Is operator and memory.py
+++ Python/Python-Fundamentals/5.Obejcts and classes/0.Is operator and memory.py
@@ -5,18 +5,17 @@ list3 = list1
 
 print(list1 == list2)       # True  - both are empty lists
 print(list1 is list2)       # False - different memory locations
-list1 += [1]                # The change will be reflected in both list1 and list3
 print(list1 is list3)       # True  - both lists are pointing to the same object - pointing to the same memory location
 print(id(list1), id(list2), id(list3))
 """
 When assigning a name of a variable in Python, you: 
 1.Create a PyObject
-2.Set the typecode to list for the PyObject
-3.Set the value to [] for the PyObject
-4.Create a name called list1
-5.Point list1 to the new PyObject
+2.Set the typecode to integer for the PyObject
+3.Set the value to 2337 for the PyObject
+4.Create a name called x
+5.Point x to the new PyObject
 
-Meaning that there are actually no exact pointers in Python
+Meaning that there are no exact pointers in Python
 https://realpython.com/pointers-in-python/   -> More Info here
 # Remember that in Python, names are just labels referencing values !!!!!!!!!!!!!!!!!!!!!!!!!!!!
 """
@@ -32,6 +31,14 @@ print(a is c)       # True  - both variables are pointing to the same object
 c += 1
 print(id(a), id(b), id(c))      # because of the last line c has a different memory address now
 
+
+print('-'*25)
+a = [1, 2]
+b = [3, 4]
+a = b                           # Now a = [3, 4] and has the same id as b
+print(id(a), id(b))             # is operator returns True
+b = [5]                         # b is reassigned to a new list and occupies a new memory location
+print(a)
 
 # Conclusion:
 # The 'is' operator tests:
